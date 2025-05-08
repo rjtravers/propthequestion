eb8b7a4e71f8424999f6bf019ff0d0ea6b83f2d2
rjtravers <38090110+rjtravers@users.noreply.github.com>
2025-05-07 20:40:12 -0400

    Update and rename script.js to firebase.js

---
+++ site/firebase.js
@@ -0,0 +1,19 @@
+// firebase.js (must be a module)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBe0YiLRSwk_qQujDG_f3aY_8nI9H_b1jc",
+  authDomain: "prop-the-question.firebaseapp.com",
+  databaseURL: "https://prop-the-question-default-rtdb.firebaseio.com",
+  projectId: "prop-the-question",
+  storageBucket: "prop-the-question.firebasestorage.app",
+  messagingSenderId: "699122354312",
+  appId: "1:699122354312:web:61e45226b38bb4cf72773d",
+  measurementId: "G-F7LQ277HDM"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
