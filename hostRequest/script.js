46e5aa987dfdca361f8013e2cd699e79f5fd19cc
rjtravers <38090110+rjtravers@users.noreply.github.com>
2025-05-08 21:50:40 -0400

    Create script.js

---
+++ hostRequest/script.js
@@ -0,0 +1,32 @@
+import { db } from './site/firebase.js';
+import { ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
+
+// Add timestamp to Firebase on button click
+document.getElementById("form_createGame").addEventListener("submit", () => {
+  const timestamp = new Date().toISOString();
+  const gameId = document.getElementById("input_gameId").value;
+  push(ref(db, "gameIds/"), { 
+        gameId: gameId,
+        timestamp: timestamp
+      })
+    .then(() => console.log("Pushed to Firebase:", gameId ))
+    .catch((err) => console.error("Firebase write failed:", err));
+});
+
+// Listen for new timestamps and add to DOM
+const timestampList = document.getElementById("timestampList");
+// const timestampsRef = ref(db, "timestamps/");
+// onChildAdded(timestampsRef, (snapshot) => {
+//   const data = snapshot.val();
+//   const li = document.createElement("li");
+//   li.textContent = data.time;
+//   timestampList.appendChild(li);
+// });
+
+const gameIds = ref(db, "gameIds/");
+onChildAdded(gameIds, (snapshot) => {
+  const data = snapshot.val();
+  const li = document.createElement("li");
+  li.textContent = data.gameId;
+  timestampList.appendChild(li);
+});
